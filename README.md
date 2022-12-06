6131de394b625b6be45bc3eb920ae4f1354773a1
karaul <karaul@ENERGY33.localdomain>
2022-12-06 19:40:13 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# boost_spsc_lockfree
+# boost_spsc_lockfree example
+
+complile:
+g++ -std=c++14 -pthread main.cpp -o main
+
+input:
+./main 1 -x -8 98 56 32 f 4 5 -89 
+
+output:
+( 1 NaN -8) 
+( 98 56 32) 
+( NaN 4 5) 
+( -89 NaN NaN) 
---
+++ main.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <iomanip>
+#include <bitset>
+#include <boost/lockfree/queue.hpp>
+//#include <atomic>
+#include <thread>
+
+#include <boost/atomic.hpp>
+
+boost::atomic_int producer_count(0);
+boost::atomic_int consumer_count(0);
+
+const int producer_thread_count = 1; //4;
+const int consumer_thread_count = 1; //4;
+
+const int ntrey=3;
+const int capacity=8;
+
+
+
+struct TreyVal
+{
+    int val=0;
+    bool flag=false; // true when healthy data, false when garbage
+};
+
+struct TreyVal producer_worker(char *c)
+{
+    try
+    {
+        int val = std::stoi(c);
+        return {val, true};
+    }
+    catch (const std::invalid_argument &ia)
+    {
+        //std::cerr << "Invalid argument: " << ia.what() << std::endl;
+        return {0, false};
+    }
+};
+
+void consumer_worker(TreyVal a, TreyVal b, TreyVal c)
+{
+
+    std::ostringstream strey;
+    strey << "(";
+    for (int k=0; k < ntrey; ++k)
+    {
+       TreyVal t = {0,false};
+        switch (k)
+        {
+        case 0:
+            t = a;
+            break;
+        case 1:
+            t = b;
+            break;
+        case 2:
+            t = c;
+            break;
+        }
+        // output "( a b c)" in strey with NaN recognition based on trey_flag
+        strey << " " << (t.flag? std::to_string(t.val) : "NaN");
+    }
+    strey  << ") ";
+    std::cout << strey.str() <<  std::endl;
+};
+
+
+int main( int argc, char *argv[] ){
+
+  boost::lockfree::queue<TreyVal> queue(capacity);
+
+   auto producer = std::thread([&]
+    {
+        TreyVal t;
+        for (int i = 1; i < argc; ++i)
+        {
+            t = producer_worker( argv[i] );
+            while (!queue.push(t));
+            //std::cout << "push:  " << t.val << ", " << t.flag << std::endl;
+
+            while ( !queue.empty() );
+        }
+    });
+
+
+   auto consumer = std::thread([&]
+    {
+        TreyVal a, b, t;
+        for (int i = 1; i < argc; ++i)
+        {
+
+            int  share_of_trey = (i%ntrey);
+            bool trey_ready = (share_of_trey==0);
+            bool trey_last = (i==argc-1);
+
+            while (!queue.pop(t));
+            //std::cout << "pop: " << t.val << ", " << t.flag << std::endl;
+
+            if(trey_ready) {
+                consumer_worker( a, b, t );
+                a={0, false};
+                b={0, false};
+            } else if (share_of_trey==1) {
+                if(trey_last) consumer_worker( t, {0, false}, {0,false} );
+                a = t;
+            } else  {
+                if(trey_last) consumer_worker( a, t, {0,false} );
+                b = t;
+            };           
+            
+        }
+    });
+
+   producer.join();
+   consumer.join();
+  
+  }
